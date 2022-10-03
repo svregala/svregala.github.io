e4c904d61aea452c79bcac76ffad90893adf83a2
Steve Regala <sregala@usc.edu>
2022-10-02 23:18:58 -0700

    Update business.js

--- hw6/business.js
+++ hw6/business.js
@@ -1,6 +1,6 @@
 /*disable location input field after checking box*/
 function location_auto_detection(){
-   document.getElementById('auto_location').onchange=function(){
+   document.getElementById('auto_location').onclick=function(){
       document.getElementById('location').disabled=this.checked;
    };
 }
