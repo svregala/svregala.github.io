68964adee03d74ec3be9699890c31631de399ca0
Steve Regala <sregala@usc.edu>
2022-10-02 23:46:56 -0700

    Update business.js

--- hw6/business.js
+++ hw6/business.js
@@ -3,5 +3,6 @@ function location_auto_detection(){
    document.getElementById('auto_location').onchange=function(){
       document.getElementById('location').disabled=this.checked;
       document.getElementById('location').value='';
+      document.getElementById('location').style.backgroundColor='lightgray';
    };
 }
