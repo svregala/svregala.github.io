2d29d25bbf6111732326d66fcd66297d7abf311b
Steve Regala <sregala@usc.edu>
2022-10-04 02:57:51 -0700

    Update business.js

--- hw6/business.js
+++ hw6/business.js
@@ -20,7 +20,7 @@ function reset_location_color(){
 
 
 
-function get_location(){
+/*function get_location(){
    var ip_json = new XMLHttpRequest();
    ip_json.open("GET", "https://ipinfo.io/json?token=1bb416f5a67c3e", true);
    ip_json.send();
@@ -28,14 +28,14 @@ function get_location(){
    console.log(JSON.parse(ip_json.responseText).loc[1]);
 
    return 1;
-}
-
-/*function get_location(){
-   var token_url = "https://ipinfo.io/json?token=1bb416f5a67c3e";
-   $.get(token_url, function(data){
-
-   })
 }*/
+
+function get_location(){
+   var token_url = "https://ipinfo.io/json?token=1bb416f5a67c3e";
+   var obj;
+   fetch(token_url).then(result=>result.json()).then(data=> obj=data);
+   console.log(obj);
+}
 
 function submitted(){
    var user_location = get_location();
