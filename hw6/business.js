a43c96ac6801beed3ca6bba5911e44bacf96f4a2
Steve Regala <sregala@usc.edu>
2022-10-05 00:33:56 -0700

    Updates

--- hw6/business.js
+++ hw6/business.js
@@ -26,32 +26,67 @@ function reset_location_color(){
    GOOGLE API KEY: AIzaSyBTLfiP5LAhW1bBZDAQaJiu-A1UfRUu7P4
 }*/
 
-/* IPInfo for auto detection for location */
+
+/* 
+** CITATION **: For the functions auto_location() & input_location(), the following website was referenced
+to invoke IPInfo API and Google Geocoding API: https://dmitripavlutin.com/javascript-fetch-async-await/.
+Lines 36-56.
+ */
+/* IPInfo for auto detection for location - AUTO DETECTION */
 async function auto_location(){
    var token_url = "https://ipinfo.io/json?token=1bb416f5a67c3e";
    var obj;
-   /*var obj;
-   fetch(token_url).then(result=>result.json()).then(data=> obj=data)
-   console.log(obj);*/
    const res = await fetch(token_url)
    obj = await res.json();
-   console.log('Coordinates', obj["loc"]);
-   var auto_location_array=obj["loc"].split(',');
-
+   var auto_location_array = obj["loc"].split(',');
+   console.log('Auto Detection Coordinates', auto_location_array);
    return auto_location_array;
 }
 
-/* Google API for location info */
-function input_location(){
+/* Google API for location info - INPUT FROM USER */
+async function input_location(){
    var google_map_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + document.getElementById('location').value + "&key=AIzaSyBTLfiP5LAhW1bBZDAQaJiu-A1UfRUu7P4";
-   console.log(google_map_url);
+   var obj;
+   const res = await fetch(google_map_url)
+   obj = await res.json();
+   var location_array=[];
+   location_array.push(obj["results"]["0"]["geometry"]["location"]["lat"], obj["results"]["0"]["geometry"]["location"]["lng"]);
+   console.log('Input From User Coordinates', location_array);
+   return location_array;
 }
 
 
 /* Main function to send to backend*/
-function submitted(){
+async function submitted(){
 
+   var yelp_url = "https://api.yelp.com/v3/businesses/search?term=";
+   var given_keyword = document.getElementById("keyword").value;
 
-   var user_location = auto_location();
-   var temp_location = input_location();
+   var given_category;
+   if(document.getElementById("category").value=='Default'){
+      given_category='all';
+   }else{
+      given_category=document.getElementById("category").value;
+   }
+
+   var given_distance;
+   if(document.getElementById('distance').value==''){
+      given_distance=parseInt(10*1609.344); // convert to meters
+   }else{
+      given_distance=parseInt(1609.344*parseInt(document.getElementById('distance').value));
+      if(given_distance>40000){
+         given_distance=40000;
+      }
+   }
+
+   var location_arr;
+   if(document.getElementById('auto_location').checked){
+      location_arr= await auto_location();
+   }else{
+      location_arr= await input_location();
+   }
+
+   yelp_url = yelp_url + given_keyword + "&latitude=" + location_arr[0] + "&longitude=" + location_arr[1] + "&categories=" + given_category + "&radius=" + given_distance;
+   console.log('YELP URL', yelp_url);
+
 }
