da6e54d2e71c6f4dfd57ba3cbb49dd4e299725d3
Steve Regala <sregala@usc.edu>
2023-01-20 16:37:54 -0800

    Update index.html

---
+++ description.js
@@ -0,0 +1,68 @@
+const about = "Hi! My name is Steve. " 
+   + "I'm currently a graduate student at the University of Southern California (USC) studying computer science. " 
+   + "I'm on my last year and last semester set to graduate in May 2023 with a Master of Science degree in computer science. "
+   + "For undergrad, I attended the University of California, Santa Barbara (UCSB) " 
+   + "and graduated with a Bachelor of Science degree in applied mathematics and a minor in statistical science. "
+   + "I enjoy the process of building software applications, and learning and applying new technologies; "
+   + "with no specified interest just yet, I'm very open to exploring and working in different parts of the software development industry"
+   + "In my free time, I like to hang out with my family and friends; we play basketball, explore new places, and eat good food."
+
+
+const educationGrad = {
+   "school" : "University of Southern California (USC)",
+   "degree" : "Master of Science in Computer Science",
+   "graduation" : "May 2023"
+}
+
+const educationUndergrad = {
+   "school" : "University of California, Santa Barbara (UCSB)",
+   "degree" : "Bachelor of Science in Applied Mathematics; minor in Statistical Science",
+   "graduation" : "June 2021"
+}
+
+const educationHS = {
+   "school" : "Homestead High School",
+   "degree" : "Not Applicable",
+   "graduation" : "August 2017"
+}
+
+const education = [educationGrad, educationUndergrad, educationHS]
+
+const project1 = {
+   "project name" : "Yelp Business Search, Responsive Web and Mobile App Development & iOS App Development",
+   "project description" : "Overall, I built a fully responsive web application to search for thousands of " 
+      + "businesses with the Yelp API using Angular, HTML, CSS, TypeScript, RxJS, and Bootstrap on the client side. "
+      + "The application is deployed and managed on Google Cloud Platform (GCP). "
+      + "The backend application is written in Node.js on the server side with the Express framework. "
+      + "The application consists of features such as navigation bar, autocomplete, routing, search form, results table, business details card, and reservation form. "
+      + "Regarding the iOS application, I utilized Swift and SwiftUI framework on XCode to build a native iOS application that allows users to perform a business search; "
+      + "with the help of the Yelp API, it provides users coherent results and informative details about the specified businesses. "
+      + "Morever, I practiced the Model-View-ViewModel (MVVM) design pattern and applied third-party libraries through Swift Package Manager such as Alamofire, Kingfisher, and SwiftyJSON.",
+   "Web link" : "https://csci-homework-8.wl.r.appspot.com/",
+   "project github iOS" : "https://github.com/svregala/Yelp_Business_Search_iOS_App_Development",
+   "project github web" : "https://github.com/svregala/Yelp_Business_Search_Responsive_Website_and_Mobile_Development",
+   "video demo iOS" : "https://youtu.be/jpORb9KFZLE",
+   "video demo web browser" : "https://youtu.be/W5lzIFnYYVU",
+   "video demo mobile browser" : "https://www.youtube.com/watch?v=fhoi_OPxm8g"
+}
+
+const project2 = {
+   "project name" : "Artificial Intelligence (AI) Projects",
+   "project description" : "Overall, I built a fully responsive web application to search for thousands of " 
+      + "businesses with the Yelp API using Angular, HTML, CSS, TypeScript, RxJS, and Bootstrap on the client side. ",
+   "project github genetic" : "https://github.com/svregala/Genetic-Algorithm-on-Travelling-Salesman-Problem-TSP",
+   "project github minmax" : "https://github.com/svregala/Minimax-Algorithm-with-Alpha-Beta-Pruning-to-play-Go-5x5",
+   "project github MLP" : "https://github.com/svregala/Multi-Layer-Perceptron-MLP-Neural-Network-from-Scratch"
+}
+
+const project3 = {
+   "project name" : "Socket Programming",
+   "project description" : "Overall, I built a fully responsive web application to search for thousands of " 
+      + "businesses with the Yelp API using Angular, HTML, CSS, TypeScript, RxJS, and Bootstrap on the client side. ",
+   "project github genetic" : "https://github.com/svregala/Genetic-Algorithm-on-Travelling-Salesman-Problem-TSP",
+   "project github minmax" : "https://github.com/svregala/Minimax-Algorithm-with-Alpha-Beta-Pruning-to-play-Go-5x5",
+   "project github MLP" : "https://github.com/svregala/Multi-Layer-Perceptron-MLP-Neural-Network-from-Scratch"
+}
+
+
+const experience = "Test"
