a09a598bc0d6048a3c9c4d8b51f39b61e4d0b85c
Steve Regala <sregala@usc.edu>
2023-01-21 19:17:35 -0800

    Final

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import education from "./description.js"
 
+window.onload = function display_aboutMe() {
+   document.getElementById("our_text").innerHTML = "This is a test INITIAL"
+}
+
+function change_text(){
+   document.getElementById('our_text').innerHTML = "This is a test"
+}
